94bdd06ca905d32091691e48406e469d77c293ed
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-10-13 11:07:56 +0300

    VMClass'lari ve UI icerisindeki Admin Area duzenlendi

---
+++ Project.MVCUI/Areas/Admin/AdminVMClasses/ProductVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.MVCUI.Areas.Admin.AdminVMClasses
+{
+    public class ProductVM //PaginationVM ile neredeyse aynı görevi yapıyor gibi gözükebilir...Aslında cok benzer görevleri yapmaktadır ancak PaginationVM sadece alısveriş tarafında kullanılacak ve sayfalandırmayı yapacak bir VM iken ProductVM sadece Admin tarafında kullanılması icin tasarlanmıs bir VM class'tır...
+    {
+    }
+}
