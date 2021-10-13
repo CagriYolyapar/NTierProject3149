beb07207f71205d1ca1c6b59da519880d10ef284
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-10-13 13:10:21 +0300

    Hesap aktivasyon tamamlandı

--- Project.MVCUI/Controllers/RegisterController.cs
+++ Project.MVCUI/Controllers/RegisterController.cs
@@ -1,4 +1,6 @@
 ﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.COMMON.Tools;
+using Project.ENTITIES.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,5 +26,64 @@ namespace Project.MVCUI.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult RegisterNow(AppUser appUser,UserProfile userProfile)
+        {
+            
+
+            appUser.Password = DantexCrypt.Crypt(appUser.Password); //sifreyi kriptoladık
+
+            //if(_apRep.Any(x=>x.UserName==appUser.UserName) || _apRep.Any(x => x.Email == appUser.Email))
+            //{
+            //    ViewBag.ZatenVar = "Kullanıcı zaten kayıtlı";
+            //    return View();
+            //}
+
+            if (_apRep.Any(x => x.UserName == appUser.UserName))
+            {
+                ViewBag.ZatenVar = "Kullanıcı ismi daha önce alınmıs";
+                return View();
+            }
+            else if(_apRep.Any(x=>x.Email == appUser.Email))
+            {
+                ViewBag.ZatenVar = "Email zaten kayıtlı";
+                return View();
+            }
+
+            //Kullanıcı basarılı bir şekilde register işlemini tamamladıysa ona mail gönder...
+            string gonderilecekEmail = "Tebrikler... Hesabınız olusturulmustur...Hesabınızı aktive etmek icin https://localhost:44347/Register/Activation/"+appUser.ActivationCode+" linkine tıklayabilirsiniz";
+
+            MailService.Send(appUser.Email, body: gonderilecekEmail, subject: "Hesap aktivasyon!!!");
+            _apRep.Add(appUser); //öncelikle bunu eklemelisiniz...Cünkü AppUser'in ID'si ilk basta olusmalı...Cünkü bizim kurdugumuz birebir ilişkide AppUser zorunlu alan Profile ise opsiyonel alandır...Dolayısıyla ilk basta AppUser'in ID'si SaveChanges ile olusmalı ki sonra Profile'i rahatca ekleyebilelim...
+            if(!string.IsNullOrEmpty(userProfile.FirstName.Trim()) ||!string.IsNullOrEmpty(userProfile.LastName.Trim()))
+            {
+                userProfile.ID = appUser.ID;
+                _proRep.Add(userProfile);
+            }
+
+            return View("RegisterOK");
+        }
+
+        public ActionResult Activation(Guid id)
+        {
+            AppUser aktifEdilecek = _apRep.FirstOrDefault(x => x.ActivationCode == id);
+            if (aktifEdilecek != null)
+            {
+                aktifEdilecek.Active = true;
+                _apRep.Update(aktifEdilecek);
+                TempData["HesapAktifMi"] = "Hesabınız aktif hale getirildi";
+                return RedirectToAction("Login", "Home");
+            }
+            TempData["HesapAktifMi"] = "Hesabınız bulunamadı";
+            return RedirectToAction("Login", "Home");
+        }
+        
+        public ActionResult RegisterOK()
+        {
+            return View();
+        }
     }
 }
+
+                 
