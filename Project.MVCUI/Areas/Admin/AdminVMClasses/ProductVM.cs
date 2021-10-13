b6f71d8e38cc280aba83cd8585bddbb6ab1d424e
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-10-13 11:09:00 +0300

    ProductVM icerisine Property'ler eklendi...

--- Project.MVCUI/Areas/Admin/AdminVMClasses/ProductVM.cs
+++ Project.MVCUI/Areas/Admin/AdminVMClasses/ProductVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project.ENTITIES.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,9 @@ namespace Project.MVCUI.Areas.Admin.AdminVMClasses
 {
     public class ProductVM //PaginationVM ile neredeyse aynı görevi yapıyor gibi gözükebilir...Aslında cok benzer görevleri yapmaktadır ancak PaginationVM sadece alısveriş tarafında kullanılacak ve sayfalandırmayı yapacak bir VM iken ProductVM sadece Admin tarafında kullanılması icin tasarlanmıs bir VM class'tır...
     {
+        public List<Product> Products { get; set; }
+        public Product Product { get; set; }
+        public List<Category> Categories { get; set; }
+
     }
 }
