f41148a618d1ecd58511b657df069c37842579f0
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-10-14 12:34:41 +0300

    Js islemleri tasarlanıyor

--- Project.MVCUI/Areas/Admin/AdminScripts/AdminCategoryDTJ.js
+++ Project.MVCUI/Areas/Admin/AdminScripts/AdminCategoryDTJ.js
@@ -2,9 +2,9 @@
 function fnFormatDetails(oTable, nTr) {
     var aData = oTable.fnGetData(nTr);
     var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
-    sOut += '<tr><td>Rendering engine:</td><td>' + aData[1] + ' ' + aData[4] + '</td></tr>';
-    sOut += '<tr><td>Link to source:</td><td>Could provide a link here</td></tr>';
-    sOut += '<tr><td>Extra info:</td><td>And any further details here (images etc)</td></tr>';
+    sOut += '<tr><td>Urunleri:</td><td> <a title="Icerik" href="/Admin/Product/ProductList/'+aData[1]+'">'+aData[2]+' ürünleri </a>     </td></tr>';
+    sOut += '<tr><td>Tek Görünüm:</td><td><a title="Detay" href="/Admin/Category/CategoryList/'+aData[1]+'">'+aData[2]+' </a> </td></tr>';
+    sOut += '<tr><td>Kategori Ekleme:</td><td>And any further details here (images etc)</td></tr>';
     sOut += '</table>';
 
     return sOut;
